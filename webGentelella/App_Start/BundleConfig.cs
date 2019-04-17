f5e5dd93993ec06bfa9d07061edc4bec4c61c7bc
PENG,CHAO-WEI <alanwei1202@gmail.com>
2019-04-17 23:23:26 +0800

    add ace admin css and js, but font icon didn't display well.

--- webGentelella/App_Start/BundleConfig.cs
+++ webGentelella/App_Start/BundleConfig.cs
@@ -14,6 +14,29 @@ namespace webGentelella
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            //ace js
+            //head
+            bundles.Add(new ScriptBundle("~/bundles/acehead").Include(
+                "~/Scripts/AceAdmin/js/ace-extra.min.js"
+                //, "~/Scripts/AceAdmin/js/"
+                ));
+            //body
+            bundles.Add(new ScriptBundle("~/bundles/acebody").Include(
+                "~/Scripts/AceAdmin/js/jquery-2.1.4.min.js"
+                , "~/Scripts/AceAdmin/js/bootstrap.min.js"
+                , "~/Scripts/AceAdmin/js/jquery-ui.custom.min.js"
+                , "~/Scripts/AceAdmin/js/jquery.ui.touch-punch.min.js"
+                , "~/Scripts/AceAdmin/js/jquery.easypiechart.min.js"
+                , "~/Scripts/AceAdmin/js/jquery.sparkline.index.min.js"
+                , "~/Scripts/AceAdmin/js/jquery.flot.min.js"
+                , "~/Scripts/AceAdmin/js/assets/js/jquery.flot.pie.min.js"
+                , "~/Scripts/AceAdmin/js/jquery.flot.resize.min.js"
+                , "~/Scripts/AceAdmin/js/ace-elements.min.js"
+                , "~/Scripts/AceAdmin/js/ace.min.js"
+                //, "~/Scripts/AceAdmin/js/"
+                ));
+
+
             // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
             // 準備好可進行生產時，請使用 https://modernizr.com 的建置工具，只挑選您需要的測試。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -25,6 +48,19 @@ namespace webGentelella
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            //ace css
+            bundles.Add(new StyleBundle("~/Content/ace").Include(
+                  "~/Content/AceAdmin/css/bootstrap.min.css"
+                , "~/Content/AceAdmin/font-awesome/4.5.0/css/font-awesome.min.css"
+                , "~/Content/AceAdmin/css/fonts.googleapis.com.css"
+                , "~/Content/AceAdmin/css/ace.min.css"
+                , "~/Content/AceAdmin/css/ace-skins.min.css"
+                , "~/Content/AceAdmin/css/ace-rtl.min.css"
+                //, "~/Content/AceAdmin/css/"
+                ));
+
+            //BundleTable.EnableOptimizations = true;
         }
     }
 }
